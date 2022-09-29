4a643f07e90234d564f931ae8871c7cf0f2bbbbb
atulsaigal <atulsaigal18@gmail.com>
2022-09-29 14:21:06 +0530

    delete
    delete

--- netcoreWebAppMVC/NetCoreWebAppWithMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NetCoreWebAppWithMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- netcoreWebAppMVC/netcoreWebAppMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace netcoreWebAppMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
